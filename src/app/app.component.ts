fcc094e43ae44782acb7e9b7e270da9d951807ea
Richard Kanigoski <richardkanigoski1@gmail.com>
2018-10-24 00:08:12 -0200

    Commit Inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogContent } from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'base';
+  constructor (public dialog: MatDialog) {}
+
+    openDialog() {
+      const dialogRef = this.dialog.open(AppComponent, {
+        height: '350px'
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('Resultado: ${result}');
+      });
+    }
+  }
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
